62bc55bc45bd9089ef109b98a6489e7652b8baee
underkim <rlaehdgns4568@gmail.com>
2022-09-28 21:21:49 +0900

    올림픽 업그레이드

--- 8979번/M8979.java
+++ 8979번/M8979.java
@@ -6,7 +6,7 @@ public class M8979 {
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in); 
         int country_n=scanner.nextInt();
-         int country = scanner.nextInt();
+         int country = scanner.nextInt()-1;
          int[] gold = new int[country_n],silver=new int[country_n],bronze=new int[country_n];
         int cnt =1;
          for (int i = 0 ; i< country_n;i++){
@@ -15,18 +15,21 @@ public class M8979 {
             silver[cou-1] = scanner.nextInt();
             bronze[cou-1] = scanner.nextInt();
          }
-         for(int i = 0 ; i < country;i++)
+         
+         for(int i = 0 ; i < country_n;i++)
          {
-            if(gold[i]>gold[country]){
+            if(gold[i]>gold[country])
                 cnt++;
-            }
-            else if(gold[i]==gold[country])
-            {
-                if(silver[i]>silver[country]) cnt++;
-                else if(silver[i]==silver[country]){
-                    if(bronze[i]>bronze[country])cnt++;
-                }
-            }   
+            
+            else if(gold[i]==gold[country] && silver[i]>silver[country])
+            
+                 cnt++; 
+                
+                
+               
+            else if(gold[i]==gold[country] && silver[i] == silver [country]&& bronze[i]>bronze[country])
+                cnt ++;
+            
          }
          System.out.println(cnt);
 
