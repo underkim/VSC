c783a341af2400ef2b7987b6b9e81cff4e26639c
underkim <rlaehdgns4568@gmail.com>
2022-09-30 19:36:17 +0900

    dfs 구현

---
+++ 2606번/M2606.java
@@ -0,0 +1,31 @@
+public class M2606 {
+    static int dfscount;
+
+		public static void main(String[] args) {
+
+			Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		boolean arr [] []= new boolean[n][n];
+		boolean chk [] = new boolean [n];
+		
+		for(int i = 0 ; i < m;i++) {
+			int x = scanner.nextInt()-1;
+			int y= scanner.nextInt()-1;
+			arr[x][y] = arr[y][x] = true;								
+												
+		}
+		
+		int cnt = 0;
+		for(int i=0;i<n;i++)
+		{ 	
+			if(!chk[i]){
+			dfs(arr,i,chk);
+			cnt++;
+		}
+
+		}
+		System.out.println(cnt);
+    
+}
