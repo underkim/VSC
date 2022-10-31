abe1cac0491f5b933bfaa4cbfa3e399a3b172000
underkim <rlaehdgns4568@gmail.com>
2022-10-31 10:56:45 +0900

    이분탐색 알고리즘 작성

--- 랜선자르기/Main.java
+++ 랜선자르기/Main.java
@@ -11,29 +11,18 @@ public class Main {
 
         int k = Integer.parseInt(s[0]);
         int n = Integer.parseInt(s[1]);
-        int line[] = new int [k];
-        int sum = 0;
+        long line[] = new long [k];  //
+        long max =0; //이분탐색 시작 끝값
         for(int i = 0; i<k;i++)
         {
             line[i]=Integer.parseInt(br.readLine());
-            sum +=line[i];
+            if(max < line[i])max=line[i];
         }
-        
-        int max = sum /n;
-        int res=0;
-        for(int i = max;i>0;i--)
-        {
-            int cnt= 0;
-            for(int j =0; j<k;j++)
-            {
-                cnt += line[j]/i;
-            }
-            if(cnt==n){
-                res = i;
-                break;
-            }
-        }
-        bw.write(res + "");
+
+
+        //이분탐색 하여 길이를 나누어 n개로 만들었을 때 경우의 수 
+       
+        //bw.write(res + "");
         bw.flush();
     }
 }
